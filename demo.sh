7beed8c9ad4dac36c9b476e4dd31fa2dcbeef78d
Kyuhyoung Choi <kyuhyoung@gmail.com>
2021-05-13 16:15:50 +0900

    Update shell script.

--- demo.sh
+++ demo.sh
@@ -1,2 +1,4 @@
-#./concatenate_img_seqs_into_video_cpp -1 output/ 1 1 0.01 128 100 4500 ../../superglue/output_seq/bracket/superglue/original/superglue_match_threshold_0.2/ ../../superglue/output_seq/bracket/loftr/original ../../superglue/output_seq/bracket/superglue/4_rotations ../../superglue/output_seq/bracket/loftr/4_rotations/
-./concatenate_img_seqs_into_video_cpp -1 output/ 1 1 0.01 128 100 4500 ../../superglue/output_seq/bice/superglue/original/bice_superglue_matchthres_0.2/ ../../superglue/output_seq/bracket/loftr/original ../../superglue/output_seq/bracket/superglue/4_rotations ../../superglue/output_seq/bracket/loftr/4_rotations/
+# bracket
+./concatenate_img_seqs_into_video_cpp -1 output/ 1 1 0.01 128 0 4500 ../../superglue/output_seq/bracket/superglue/original/superglue_match_threshold_0.2/ ../../superglue/output_seq/bracket/loftr/original ../../superglue/output_seq/bracket/superglue/4_rotations ../../superglue/output_seq/bracket/loftr/4_rotations/
+# bice
+#./concatenate_img_seqs_into_video_cpp -1 output/ 1 1 0.01 128 100 4500 ../../superglue/output_seq/bice/superglue/original/bice_superglue_matchthres_0.2/ ../../superglue/output_seq/bice/loftr/original ../../superglue/output_seq/bice/superglue/4_rotations ../../superglue/output_seq/bice/loftr/4_rotations/
